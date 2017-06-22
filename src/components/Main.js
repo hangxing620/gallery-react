746e1f73b885396e9fe9a586f40a5c6b2e19652b
bellhang <1204662932@qq.com>
2017-06-22 09:37:42 +0800

    is ok

--- src/components/Main.js
+++ src/components/Main.js
@@ -67,8 +67,8 @@ class ImgFigure extends React.Component{
 
 		// 如果图片的旋转角度有值并且不为0，添加旋转角度
 		if (this.props.arrange.rotate) {
-			(['-moz-', '-ms-', '-webkit-', '']).forEach((value) => {
-				styleObj[`${value}transform`] = `rotate(${this.props.arrange.rotate}deg)`;
+			(['MozTransform', 'msTransform', 'WebkitTransform', 'transform']).forEach((value) => {
+				styleObj[value] = `rotate(${this.props.arrange.rotate}deg)`;
 			})
 			
 		}
@@ -101,11 +101,30 @@ class ControllerUnit extends React.Component {
 	handleClick(e) {
 		e.stopPropagation();
 		e.preventDefault();
+
+		// 如果点击的是当前正在选中态的按钮，则翻转图片，否则将对应的图片居中
+		if (this.props.arrange.isCenter) {
+			this.props.inverse();
+		} else {
+			this.props.center();
+		}
+		
 	}
 
 	render() {
+		let controllerUnitClassName = 'controller-unit';
+
+		// 如果对应的是居中的图片，显示控制按钮的居中态
+		if (this.props.arrange.isCenter) {
+			controllerUnitClassName += ' is-center';
+
+			// 如果同时对应的是翻转图片，显示控制的翻转态
+			if (this.props.arrange.isInverse) {
+				controllerUnitClassName += ' is-inverse';
+			}
+		}
 		return (
-			<span className="controller-unit" onClick={this.handleClick.bind(this)}></span>
+			<span className={controllerUnitClassName} onClick={this.handleClick.bind(this)}></span>
 		);
 	}
 }
@@ -181,7 +200,7 @@ class AppComponent extends React.Component {
 			vPosRangeX = vPosRange.x,
 
 			imgsArrangeTopArr = [],
-			topImgNum = Math.ceil(Math.random() * 2),  // 取一个或者不取
+			topImgNum = Math.floor(Math.random() * 2),  // 取一个或者不取
 			topImgSpliceIndex = 0,
 
 			imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
@@ -316,7 +335,10 @@ class AppComponent extends React.Component {
 			imgFigures.push(<ImgFigure data={value} key={index}
 				ref={`imgFigure${index}`} arrange={this.state.imgsArrangeArr[index]}
 				inverse={this.inverse(index)} center={this.center(index)}/>);
-			controllerUnits.push(<ControllerUnit key={index}/>)
+			controllerUnits.push(<ControllerUnit key={index}
+			arrange={this.state.imgsArrangeArr[index]}
+			inverse={this.inverse(index)}
+			center={this.center(index)}/>)
 		})
 
     return (
